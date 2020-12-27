4b0504c591641f90583efbb6ed4cb8e17782fb34
py-rohit <askpygod@gmail.com>
2020-12-27 14:14:16 +0530

    list of guilds, connected to psql

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from oauth.client import DiscordOauth2Client
 from quart import Quart, redirect, request, render_template
 from config import *
+import asyncpg
+import asyncio
 
 app = Quart(__name__)
 app.secret_key = quart_secret
@@ -28,8 +30,13 @@ async def callback():
     code = request.args.get('code')
     access_token = await client.get_access_token(code)
     guilds = await client.fetch_guilds(access_token)
-    # return render_template("guilds.html", glist=guilds)
-    return str(guilds)
+    return await render_template("guilds.html", glist=guilds)
 
+
+async def create_db_pool():
+    conn = await asyncpg.connect(database="QuotientDB", user="postgres", password="588y5vwa")
+    print("--DB Connection Complete --")
+
+asyncio.get_event_loop().run_until_complete(create_db_pool())
 
 app.run(debug=True, port=5000, host='localhost')
--- test.py
+++ test.py
@@ -5,14 +5,18 @@ songs = [
     {"title": "Human Touch", "playcount": 3}
 ]
 
-print("===========================")
-print(f'Songs --> {songs} \n')
-title = list(map(lambda x: x['title'], songs))
-print(f'Print Title --> {title}')
+# print("===========================")
+# print(f'Songs --> {songs} \n')
+# title = list(map(lambda x: x['title'], songs))
+# print(f'Print Title --> {title}')
 
-playcount = list(map(lambda x: x['playcount'], songs))
-print(f'Print Playcount --> {playcount}')
-print(f'Print Sorted playcount --> {sorted(playcount)}')
+# playcount = list(map(lambda x: x['playcount'], songs))
+# print(f'Print Playcount --> {playcount}')
+# print(f'Print Sorted playcount --> {sorted(playcount)}')
 
-# Aliter -
-print(sorted(list(map(lambda x: x['playcount'], songs))))
+# # Aliter -
+# print(sorted(list(map(lambda x: x['playcount'], songs))))
+
+for i in songs:
+    print('title = ', i['title'], '\nplaycount = ', i['playcount'])
+# print('playcount = ', i['playcount'])
